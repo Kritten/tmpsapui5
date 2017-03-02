6b13bdc993f233c5d1c1fb0c29595bac363304d7
Kristof Komlòssy <kristof.komlossy@bpc.ag>
2017-03-02 16:08:30 +0100

    Hintergrund Bild, alt Nutzungsart raus aus KE MFs

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ $(function() {
     sap.ui.getCore().attachInit(function () {
         new sap.m.Shell({
             appWidthLimited: true,
+            backgroundImage: './img/shell_bg.jpg',
+            backgroundOpacity: 0.2,
             app: new sap.ui.core.ComponentContainer({
                 name : "ag.bpc.Deka",
                 height : "100%"
