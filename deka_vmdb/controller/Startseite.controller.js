8c0492781a631f4d30ee2faa6c871bb7d92eac12
Kristof Komlòssy <kristof.komlossy@bpc.ag>
2017-04-02 19:17:17 +0200

    Kachel Ausblenden für Fond- oder Assetmanager

--- deka_vmdb/controller/Startseite.controller.js
+++ deka_vmdb/controller/Startseite.controller.js
@@ -1,12 +1,34 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"ag/bpc/Deka/util/StaticData",
+	"ag/bpc/Deka/util/ErrorMessageUtil"], function (Controller, StaticData, ErrorMessageUtil) {
 	
 	"use strict";
 	return Controller.extend("ag.bpc.Deka.controller.Startseite", {
 		
 		onInit: function(evt){
-			this.getView().setModel(sap.ui.getCore().getModel("i18n"), "i18n");			
+			this.getView().setModel(sap.ui.getCore().getModel("i18n"), "i18n");
+
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("startseite").attachPatternMatched(this.onPatternMatch, this);
 		},
         
+		onPatternMatch: function(oEvent){
+			var _this = this;
+
+			StaticData.USER.then(function(user){
+				_this.getView().byId('idKonditioneneinigungTile').setVisible(true);
+				_this.getView().byId('idVermietungsaktivitaetTile').setVisible(true);
+				_this.getView().byId('idBudgetstoppTile').setVisible(!user.BtnAm);
+				_this.getView().byId('idSperreTile').setVisible(!user.BtnFm);
+			})
+            .catch(function(oError){
+                console.log(oError);
+                ErrorMessageUtil.showError(oError);
+            })
+            .done();
+		},
+
 		// Kacheln vom Startbildschirm werden angeklickt    
 		onKondTilePress: function(evt) {
 			// Navigation zur der View
