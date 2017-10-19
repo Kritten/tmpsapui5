53e609e508588d93684f2eb7250835ea7f5a69bd
Kristof Komlòssy <kristof.komlossy@bpc.ag>
2017-10-19 10:05:38 +0200

    Leerzeichen

--- deka_vmdb/util/PrinterUtil.js
+++ deka_vmdb/util/PrinterUtil.js
@@ -317,7 +317,7 @@ sap.ui.define(["ag/bpc/Deka/util/PrinterUtil",
 
                         keMappingHtml += _.join(_.map(_.filter(vermietungsaktivitaet.VaToMap, function(ke){return ke.Aktiv;}), function(ke){
                             return ke.KeId;
-                        }), ",");
+                        }), ", ");
 
                         keMappingHtml += "</td>";
                         result = result.replace("@@KEMapping@@", keMappingHtml);
