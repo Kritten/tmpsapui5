145e1d563a1f35b56c5ec14c91bccf0c72ebe85a
Kristof Komlòssy <kristof.komlossy@bpc.ag>
2018-07-31 14:05:23 +0200

    Code Style

--- deka_vmdb/controller/Startseite.controller.js
+++ deka_vmdb/controller/Startseite.controller.js
@@ -1,24 +1,25 @@
 /*
- * @Author: Christian Hoff (best practice consulting AG) 
- * @Date: 2017-04-05 21:43:42 
- * @Last Modified by:   Christian Hoff (best practice consulting AG) 
- * @Last Modified time: 2017-04-05 21:43:42 
+ * @Author: Christian Hoff (best practice consulting AG)
+ * @Date: 2017-04-05 21:43:42
+ * @Last Modified by:   Christian Hoff (best practice consulting AG)
+ * @Last Modified time: 2017-04-05 21:43:42
  */
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"ag/bpc/Deka/util/StaticData",
-	"ag/bpc/Deka/util/ErrorMessageUtil"], function (Controller, StaticData, ErrorMessageUtil) {
-	
+	"ag/bpc/Deka/util/ErrorMessageUtil"
+], function (Controller, StaticData, ErrorMessageUtil) {
+
 	"use strict";
 	return Controller.extend("ag.bpc.Deka.controller.Startseite", {
-		
+
 		onInit: function(evt){
 			this.getView().setModel(sap.ui.getCore().getModel("i18n"), "i18n");
 
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.getRoute("startseite").attachPatternMatched(this.onPatternMatch, this);
 		},
-        
+
 		onPatternMatch: function(oEvent){
 			var _this = this;
 
@@ -35,26 +36,26 @@ sap.ui.define([
             .done();
 		},
 
-		// Kacheln vom Startbildschirm werden angeklickt    
+		// Kacheln vom Startbildschirm werden angeklickt
 		onKondTilePress: function(evt) {
 			// Navigation zur der View
 			this.getOwnerComponent().getRouter().navTo("konditioneneinigungSelektion");
 		},
-		
+
 		onVermTilePress: function(evt) {
 			// Navigation zur der View
 			this.getOwnerComponent().getRouter().navTo("vermietungsaktivitaetSelektion");
 		},
-		
+
 		onBudsTilePress: function(evt) {
 			// Navigation zur der View
 			this.getOwnerComponent().getRouter().navTo("budgetstopp");
 		},
-		
+
 		onSperTilePress: function(evt) {
 			// Navigation zur der View
 			this.getOwnerComponent().getRouter().navTo("sperren");
 		}
-	
+
 	});
 });
